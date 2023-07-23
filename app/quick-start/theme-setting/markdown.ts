c394b2d41d5ebfa09f337409357ac448a927d946
abdul <malik301002@gmail.com>
2023-07-23 22:15:13 +0700

    fix: grammatical errors in theme switcher page instructions

--- app/quick-start/theme-setting/markdown.ts
+++ app/quick-start/theme-setting/markdown.ts
@@ -3,7 +3,8 @@ export const markdown = `
 
 Developers love dark theme but setting up the theme can be very hectic for the developers as when there are many components in your app and you have to pass your theme props very deep in the component tree where things can become ambigous.
 
-With the help of jotai you can setup different themes for you app in minutes. Let's take a look at this,
+With the help of jotai, you can setup different themes for your app in minutes. Let's take a look at this:
+
 Initialize a theme atom with a default value. 
 
 ~~~js
