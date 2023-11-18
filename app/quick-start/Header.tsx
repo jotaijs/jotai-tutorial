9ce5a90cb82c5cf8f0be3c1ba23d71a2df911c48
lesenelir <hi@lesenelir.me>
2023-11-18 19:42:25 +0800

    fix: adapt to common mobile layout css

--- app/quick-start/Header.tsx
+++ app/quick-start/Header.tsx
@@ -2,9 +2,9 @@
 
 import React from "react";
 import Hamburger from "./Hamburger";
-import { AiFillGithub } from "react-icons/ai"
+import { AiFillGithub } from "react-icons/ai";
 import { atom, useAtom } from "jotai";
-import { BsSun, BsFillMoonStarsFill } from "react-icons/bs"
+import { BsSun, BsFillMoonStarsFill } from "react-icons/bs";
 
 const theme = atom('light');
 export const themeAtom = atom(get => get(theme), (get, set) => {
